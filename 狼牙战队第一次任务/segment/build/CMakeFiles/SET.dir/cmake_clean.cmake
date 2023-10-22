b41533743eed4e790eb8027be3e55afc2903e04d
name <zzc2697072261@outlook.com>
2023-10-22 19:02:19 +0800

    first commit

---
+++ 狼牙战队第一次任务/segment/build/CMakeFiles/SET.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SET.dir/src/main.cpp.o"
+  "CMakeFiles/SET.dir/src/main.cpp.o.d"
+  "CMakeFiles/SET.dir/src/segment.cpp.o"
+  "CMakeFiles/SET.dir/src/segment.cpp.o.d"
+  "SET"
+  "SET.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
